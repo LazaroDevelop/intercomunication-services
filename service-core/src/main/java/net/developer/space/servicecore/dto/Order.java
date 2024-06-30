fc5c3e019cbdc1d51847dddb5414e055cdbdc1d0
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-06-30 11:38:58 -0400

    do refactor

---
+++ service-core/src/main/java/net/developer/space/servicecore/dto/Order.java
@@ -0,0 +1,15 @@
+package net.developer.space.servicecore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String name;
+    private int quantity;
+    private double price;
+}
