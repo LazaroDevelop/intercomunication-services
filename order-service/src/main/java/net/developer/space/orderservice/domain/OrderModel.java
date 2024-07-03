532f1adb46f904d0b4eca4891a0d323171925cbe
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-07-03 17:54:00 -0400

    add order model

---
+++ order-service/src/main/java/net/developer/space/orderservice/domain/OrderModel.java
@@ -0,0 +1,10 @@
+package net.developer.space.orderservice.domain;
+
+import net.developer.space.servicecore.dto.OrderEvent;
+
+import java.io.Serializable;
+
+
+public class OrderModel extends OrderEvent implements Serializable {
+    private static final long serialVersionUUID = 1L;
+}
