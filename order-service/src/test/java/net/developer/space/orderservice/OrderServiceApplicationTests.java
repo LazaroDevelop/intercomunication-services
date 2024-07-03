10523a33857bc8da4e8f75bfc3cf65c7712c505e
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-07-03 17:57:41 -0400

    add unitary test support

--- order-service/src/test/java/net/developer/space/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/net/developer/space/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package net.developer.space.orderservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class OrderServiceApplicationTests {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
     void contextLoads() {
