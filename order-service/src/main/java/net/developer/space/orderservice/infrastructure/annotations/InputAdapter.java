42e653f1eb5911230fb2bb470701af27b9c20f3e
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2025-05-22 10:47:31 -0400

    update Input Adapter custom annotation

--- order-service/src/main/java/net/developer/space/orderservice/infrastructure/annotations/InputAdapter.java
+++ order-service/src/main/java/net/developer/space/orderservice/infrastructure/annotations/InputAdapter.java
@@ -1,17 +1,11 @@
 package net.developer.space.orderservice.infrastructure.annotations;
-
-import org.springframework.core.annotation.AliasFor;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.annotation.*;
 
+
+@Documented
+@RestController
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Component
-@Documented
-public @interface InputAdapter {
-
-    @AliasFor(annotation = RestController.class)
-    String value() default "";
-}
+public @interface InputAdapter {}
